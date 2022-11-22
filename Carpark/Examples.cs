c8c34e25c3bce20e684c7d7bc1898d2fec8b15f9
423locked <yaroslav05042005@gmail.com>
2022-11-22 22:13:48 +0300

    Old projects commit

--- Carpark/Examples.cs
+++ Carpark/Examples.cs
@@ -28,4 +28,44 @@ public static class Examples
         Carpark carpark = new Carpark("Autopark", vehicles);
         carpark.PrintVehicles();
     }
+    
+    public static void Test2_2()
+    {
+        Homework.Hometasks.Task2_2(5, 12);
+    }
+    
+    public static void Test2_3_4()
+    {
+        Homework.Hometasks.Task2_3(1, 2, 4, 8);
+        Homework.Hometasks.Task2_4(2251816993685505);
+    }
+
+    public static void Test1_3()
+    {
+        Homework.Hometasks.Task1_3(0, 0, 0, 4, 4, 4);
+        Homework.Hometasks.Task1_3(4, 0, 0, 4, 4, 4);
+        Homework.Hometasks.Task1_3(0, 0, 4, 0, 0, 4);
+        Homework.Hometasks.Task1_3(0, 0, 4, 0, 4, 4);
+    }
+
+    public static void Test3_1()
+    {
+        int[] a = {1, 1, 1, 1};
+        int p = 2;
+        Console.WriteLine(Homework.Hometasks.Task3_1(p, a));
+    }
+    
+    public static void Test3_2()
+    {
+        int[] a = {5, 6, 0, -1, 3, 9, 4, 4, 4, 8};
+        Console.WriteLine(Homework.Hometasks.Task3_2(2, a));
+    }
+
+    public static void Test3_3()
+    {
+        int[] a = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        string query = "2:10:-2";
+        foreach (var el in Homework.Hometasks.Task3_3(a, query))
+            Console.WriteLine($"{el} ");
+    }
 }
